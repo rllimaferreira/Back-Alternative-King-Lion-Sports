a77e8ccd888666105715b4cfa37b1d5c7381869f
Felipe Godoy <felipe-est@hotmail.com>
2022-09-15 18:43:49 -0300

    Implemented Controllers, DTOs and Service classes

--- src/main/java/br/com/dh/ecommerce/dtos/CategoryDto.java
+++ src/main/java/br/com/dh/ecommerce/dtos/CategoryDto.java
@@ -1,10 +1,42 @@
 package br.com.dh.ecommerce.dtos;
 
+import br.com.dh.ecommerce.entities.Category;
+
 import java.io.Serializable;
 
 public class CategoryDto implements Serializable {
-    private static final long serialVersionUID = 1L;
 
+    private static final long  serialVersionUID = 1L;
 
+    private Integer id;
+    private String name;
 
+    public CategoryDto() {
+    }
+
+    public CategoryDto(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDto(Category category) {
+        this.id = category.getId();
+        this.name = category.getName();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,15 @@
 -- AQUI TEMOS O SEEDING DE DADOS
 
---Produto(Product)
-INSERT INTO product (title, description, brand, image, price) VALUES ('Produto teste', 'Descrição teste','Nike', 'https://imgnike-a.akamaihd.net/1300x1300/01091215.jpg', '99.90');
-
 --Categoria(Category)
 INSERT INTO category (name) VALUES ('Camiseta');
+
+--Produto(Product)
+INSERT INTO product (title, description, brand, image, price) VALUES ('Produto teste', 'Descrição teste','Nike', 'https://imgnike-a.akamaihd.net/1300x1300/01091215.jpg', 99.90);
+
+--Product_category
+INSERT INTO product_category (product_id, category_id) VALUES (1, 1);
+
+
 
 --Cliente (Client) - Precisa criar o mvc completo
     -- INSERT INTO Client (email, fullname, birthdate, cpf, city, postalcode, state, street, residencenumber, residencecomplement, creationdate)
